fa5c671a2f2ded82039f0774beabb7e7948ad2af
pandianpdy <pandianpdy@gmail.com>
2017-03-20 07:24:56 +0530

    MyBank Modi - 20-03-2017

--- JavaProj/Bank/MyBank.java
+++ JavaProj/Bank/MyBank.java
@@ -63,41 +63,58 @@ class MyBank {
         System.out.println("\n\n----------------------------------");
         System.out.println("              Bank Master");
         System.out.println("----------------------------------\n");
+        String dbnkname = sc.nextLine();
         System.out.print("              Bank Name : ");
-        String bnkname = sc.next();
-        System.out.print("\n              Address   : ");
-        String bnkaddress = sc.next();
-        System.out.print("\n              IFC Code  : ");
+        String bnkname = sc.nextLine();
+        System.out.print("              Address   : ");
+        String bnkaddr = sc.nextLine();
+        System.out.print("              IFC Code  : ");
         String bnkifc = sc.next();
-        System.out.println("----------------------------------\n");        
+        System.out.println("\n----------------------------------\n");        
+        System.out.println(bnkname);
+        System.out.println(bnkaddr);
+        System.out.println(bnkifc);
+        
     }
     public void getCustomer(){
         System.out.println("\n\n----------------------------------");
         System.out.println("              Customer Master");
         System.out.println("----------------------------------\n");
+        sc.nextLine();
         System.out.print("              Customer Name : ");
-        String custname = sc.next();
-        System.out.print("\n              Address   : ");
-        String custaddress = sc.next();
-        System.out.print("\n              Bank Name  : ");
-        String bnkname = sc.next();
-        System.out.print("\n              Account No.  : ");
+        String custname = sc.nextLine();
+        System.out.print("              Address   : ");
+        String custaddress = sc.nextLine();
+        System.out.println("              Bank Name   ");
+        System.out.println("              1. ICICI Bank");
+        System.out.println("              2. Canara Bank");        
+        System.out.println("              3. SBI");        
+        System.out.print  ("                 Bank Code : ");        
+        int bnkname = sc.nextInt();
+        System.out.print("              Account No.  : ");
         String accountno = sc.next();
-        System.out.println("----------------------------------\n");        
+        System.out.println("\n----------------------------------\n");        
     }
     public void doTransaction(){
         System.out.println("\n\n----------------------------------");
         System.out.println("              Transaction Master");
         System.out.println("----------------------------------\n");
-        System.out.print("              Bank Code : ");
-        String bnkname = sc.next();
-        System.out.print("\n              Account No.   : ");
-        String accountno = sc.next();
-        System.out.print("\n              Date (dd/mm/yyyy) : ");
+        System.out.println("               Bank Name   ");
+        System.out.println("                 1. ICICI Bank");
+        System.out.println("                 2. Canara Bank");        
+        System.out.println("                 3. SBI");                
+        System.out.print  ("                    Bank Code : ");
+        int bnkcode = sc.nextInt();
+        System.out.println("               ICICI Bank A/C No.   ");
+        System.out.println("                 1. 873837409");
+        System.out.println("                 2. 99837464");        
+        System.out.print  ("                    Account Code.   : ");
+        int accode = sc.nextInt();
+        System.out.print("              Date (dd/mm/yyyy) : ");
         String date = sc.next();
-        System.out.print("\n              Deposit/Withdrawal  : ");
+        System.out.print("              Deposit/Withdrawal (d/w) : ");
         char isdeposit = sc.next(".").charAt(0);
-        System.out.print("\n              Amount  : ");
+        System.out.print("              Amount  : ");
         double amount = sc.nextDouble();        
         System.out.println("----------------------------------\n");        
     }
